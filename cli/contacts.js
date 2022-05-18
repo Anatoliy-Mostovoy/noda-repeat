54598b37150728df23fbc1ff9a0a811079198d99
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-05-18 13:04:12 +0300

    read file with fs module  + try...catch

--- cli/contacts.js
+++ cli/contacts.js
@@ -1,9 +1,24 @@
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
 
-console.log(__dirname);
-console.log(path.join(__dirname, "contacts.json"));
+// console.log(__dirname);
+// console.log(path.join(__dirname, "contacts.json")); //* правильное обращение к файлу
 
-const contacts = fs.readFile(path.join(__dirname, "contacts.json", "utf8"));
+const readContacts = async () => {
+  const response = await fs.readFile(
+    path.join(__dirname, "contacts.json"),
+    "utf8"
+  );
+  try {
+    const contacts = JSON.parse(response);
+    return contacts;
+  } catch (error) {
+    console.log("Ошибка  в трай кетч");
+  }
+};
 
-console.log(contacts);
+console.log(
+  readContacts().then((data) => {
+    console.log(data);
+  })
+);
