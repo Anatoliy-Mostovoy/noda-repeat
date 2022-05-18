533a0e589e59de2b8692f067706b1ac2671ad225
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-05-18 22:32:17 +0300

    Create srver on module http nodejs

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const path = require("path");
 
 const PORT = process.env.PORT || 8081;
 
-http
-  .createServer(async (req, res) => {
-    const content = await fs.readFile(path.join(__dirname, "index.html"));
-    res.writeHead(200, { "Content-type": "text/html" });
-    res.end(content);
-  })
-  .listen(PORT, () => {
-    console.log(`Server was start ${PORT}`);
-  });
+const requestHandler = async (req, res) => {
+  const content = await fs.readFile(path.join(__dirname, "index.html"));
+  res.writeHead(200, { "Content-type": "text/html" });
+  res.end(content);
+};
+
+const server = http.createServer(requestHandler);
+server.listen(PORT, (error) => {
+  if (error) {
+    return console.log("Something go wrong");
+  }
+  console.log(`Server was start ${PORT}`);
+});
