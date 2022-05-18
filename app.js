1fd7d310b28aad9c8f656252899e1363f3db0250
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-05-18 18:05:06 +0300

    create server om node js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//TODO Создаем сервер
+
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
@@ -5,9 +7,10 @@ const path = require("path");
 const PORT = process.env.PORT || 8080;
 
 http
-  .createServer((req, res) => {
+  .createServer(async (req, res) => {
+    const content = await fs.readFile(path.join(__dirname, "index.html"));
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>Hallo</h1>");
+    res.write(content);
     res.end();
   })
   .listen(PORT, () => {
