bf43a090ec27c1029656aa77562862a910d9f5d6
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-05-18 22:12:58 +0300

    clear server

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 //TODO Создаем сервер
 
 const http = require("http");
-const fs = require("fs");
+const fs = require("fs/promises"); //!! обязательно для работы с асинхронными запросами
 const path = require("path");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 http
   .createServer(async (req, res) => {
     const content = await fs.readFile(path.join(__dirname, "index.html"));
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(content);
-    res.end();
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(content);
   })
   .listen(PORT, () => {
     console.log(`Server was start on port ${PORT}`);
