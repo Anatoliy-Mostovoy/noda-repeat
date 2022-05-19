7bc5864c20d8c5cf0780c508d5ce44ae7fb4a286
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-05-19 11:02:21 +0300

    use moddlewawre morgan

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const { response } = require("express");
 const express = require("express");
 const app = express(); //* инициалищируем express приложение
+var morgan = require("morgan");
 
 const PORT = process.env.PORT || 8081;
 
 app.use(express.json()); //* мидлвар для парса json в запросе. Записывает в req.body
 app.use(express.static("public")); //* даем публичный доступ к какому-то файлу на бэке. Без указания пути. Просто имя папки от куда тянуть данные
+app.use(morgan("tiny")); //* middleware для логирования запросов
 
 app.post("/home", (req, res) => {
   console.log(req.body);
